e195a25f95e2660014c1543f072b79de6ad06f19
samwanjo41 <hulahoop41@gmail.com>
2019-01-09 22:38:05 +0300

    [finishes #163046718] modified meetups-model

--- app/api/v1/models/meetups-model.py
+++ app/api/v1/models/meetups-model.py
@@ -1,43 +1,41 @@
 """
-The admin meetup model
+meetups model
 """
 #import date
 from datetime import datetime
 
 #assign meetups to an empty list
-MEETUPS_rec = []
+meetups_rec = []
 
 #create the meetup model class
-class MeetupModel:
-    def __init__(self, topic, happenningOn, location, images, tags):
+class MeetupsModel:
+    def __init__(self):
+        self.db = meetups_rec
+
+    def create_meetups(self, topic, happenningOn, location, images, tags):
+        
         """
        Initialize the meetup class, with your variables at hand
         """
-        self.id = len(MEETUPS_LEN)+1
-        self.topic = topic
-        self.happeningOn = happenningOn
-        self.location = location
-        self.images = images
-        self.tags = tags
-        self.created_at = datetime.now()
+        payload = {
 
-    def save_meetup_record(self):
-        """
-        save a new meetup record 
-        """
-        MEETUPS_rec.append(self)
-
-    #staticmethod decorator
-    #convert the meetup record to JSON format
-    #let the dict be readable
-    @staticmethod
-    def to_json(meetup):
-        return {
-            "id": meetup.id,
-            "topic": meetup.topic,
-            "happenningOn": meetup.happenningOn,
-            "location": meetup.location,
-            "images": meetup.images,
-            "tags": meetup.tags,
-            "created_at": meetup.created_at
+        "id" = len(MEETUPS_LEN)+,
+        "topic" = topic
+        "happeningOn" = happenningOn
+        "location" = location
+        "images" = images
+        "tags" = tags
+        "created_at" = datetime.now()
         }
+    self.db.append(payload)
+        return payload
+
+    def get_all_meetups(self):
+        """Retrieves all upcoming meetups"""
+        return self.db
+
+     def get_a_specific_meetup(self, id):
+        """Retrieves a specific meetup"""
+        meetup = [new_meetup for new_meetup in self.db
+                  if new_meetup["id"] == id]
+        return meetup
