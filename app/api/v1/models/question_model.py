10413976303917ba3bfda85bf9ab02afea9b3853
samwanjo41 <hulahoop41@gmail.com>
2019-01-11 04:33:38 +0300

    Added question model and view file

--- app/api/v1/models/question_model.py
+++ app/api/v1/models/question_model.py
@@ -1,21 +1,26 @@
+from datetime import datetime
+
 questions_list = []
 
-class quiz(object):    
+
+class QuestionsModel(object):    
     def __init__(self):
         self.db = questions_list
     
-    def create_questions(self, title, question, user, meetup):
-        query = {
+    def create_questions(self, title, body, meetup, votes, createdBy):
+        payload = {
 
-        "questionId" = len(self.db)+1
-        "user" = user
-        "meetup" = meetup
-        "title" = title
-        "question" = question
+            "id": len(self.db) + 1,
+            "createdOn": datetime.now(),
+            "createdBy": createdBy,
+            "meetup": meetup,
+            "title": title,
+            "body": body,
+            "votes": votes
         }
 
-        self.db.append(query)
-            return query
+        self.db.append(payload)
+        return payload
 
     def get_all_questions(self):
         """Retrieves all questions"""
@@ -24,7 +29,7 @@ class quiz(object):
     def get_a_specific_question(self, id):
         """Retrieves a specific question"""
         question = [new_quiz for new_quiz in self.db
-                  if new_quiz["id"] == id]
+            if new_quiz["id"] == id]
         return meetup
 
     
