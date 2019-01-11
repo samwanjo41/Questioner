b68d17ae9ab71758f36202ea2bf134ca964196a6
samwanjo41 <hulahoop41@gmail.com>
2019-01-11 04:24:47 +0300

    Modifie init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask
 
 from config import app_config
+from .api.v1.views.user_view import method1
+from .api.v1.views.meetups_view import version2
+from .api.v1.views.question_view import app1
 
 
 def create_app(config_name):
@@ -10,5 +13,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
-
+    app.register_blueprint(method1) 
+    app.register_blueprint(version2) 
+    app.register_blueprint(app1) 
     return app
