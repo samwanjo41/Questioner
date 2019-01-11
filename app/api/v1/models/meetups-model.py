b1fe8b99d321dae0f07fe5fcea5c98d572c75e14
samwanjo41 <hulahoop41@gmail.com>
2019-01-11 03:54:44 +0300

    [finishes #163046872] Model file for meetups

--- app/api/v1/models/meetups-model.py
+++ app/api/v1/models/meetups-model.py
@@ -1,40 +1,42 @@
 """
 meetups model
 """
-#import date
+# import date
 from datetime import datetime
 
-#assign meetups to an empty list
+# assign meetups to an empty list
 meetups_rec = []
 
-#create the meetup model class
-class MeetupsModel:
+# create the meetup model class
+
+
+class MeetupsModel():
+    
     def __init__(self):
         self.db = meetups_rec
 
-    def create_meetups(self, topic, happenningOn, location, images, tags):
+    def create_meetups(self, topic, happeningOn, location, images, tags):
         
         """
        Initialize the meetup class, with your variables at hand
         """
         payload = {
-
-        "id" = len(MEETUPS_LEN)+,
-        "topic" = topic
-        "happeningOn" = happenningOn
-        "location" = location
-        "images" = images
-        "tags" = tags
-        "created_at" = datetime.now()
+            "id": len(self.db) + 1,
+            "topic": topic,
+            "happeningOn": happeningOn,
+            "location": location,
+            "images": images,
+            "tags": tags,
+            "createdOn":  datetime.now()
         }
-    self.db.append(payload)
+        self.db.append(payload)
         return payload
 
     def get_all_meetups(self):
-        """Retrieves all upcoming meetups"""
+        """Retrieves all  meetups"""
         return self.db
 
-     def get_a_specific_meetup(self, id):
+    def get_a_specific_meetup(self, id):
         """Retrieves a specific meetup"""
         meetup = [new_meetup for new_meetup in self.db
                   if new_meetup["id"] == id]
