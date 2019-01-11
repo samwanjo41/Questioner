2402072c8ad56ef464ca40e41bce32130d06a96a
samwanjo41 <hulahoop41@gmail.com>
2019-01-11 03:04:30 +0300

    [finishes #163046972] nodified user view and model file

--- app/api/v1/views/user_view.py
+++ app/api/v1/views/user_view.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, jsonify, make_response, request, flask
-from app.api.v1.user_model import UsersModel
+from flask import Blueprint, jsonify, make_response, request
+from ..models.user_model import UsersModel
+
+user_view = UsersModel()
 
 method1 = Blueprint('api', __name__,)
 
-@method1.route('/api/vi/register', methods=['POST'])
+
+@method1.route('/api/v1/register', methods=['POST'])
 def register():
     try:
         data = request.get_json()
-        firstname = data["First Name"]
+        firstname = data["FirstName"]
         lastname = data["LastName"]
         othername = data["othername"]
         email = data["email"]
@@ -18,56 +21,27 @@ def register():
         return jsonify({
             "Error": "Invalid {} Key field".format(e)
         }), 400
-
-    if not check_name_format(firstname):
-        return make_response(jsonify({
-            "Error": "firstname is invalid"
-        }), 400)
-
-    if not check_name_format(lastname):
-        return make_response(jsonify({
-            "Error": "lastname  is invalid"
-        }), 400)
-
-    if not check_name_format(othername):
-        return make_response(jsonify({
-            "Error": "othername is invalid"
-        }), 400)
     
-    if not check_name_format(email):
-        return make_response(jsonify({
-            "Error": "email is invalid"
-        }), 400)
-
-    if not check_name_format(username):
-        return make_response(jsonify({
-            "Error": "username is invalid"
-        }), 400)
-    
-    if not check_name_format(phonenumber):
-        return make_response(jsonify({
-            "Error": "phonenumber is invalid"
-        }), 400)
-    
-    if not check_name_format(password):
-        return make_response(jsonify({
-            "Error": "password is invalid"
-        }), 400)
-
-
-
-
-
-    if check_for_empty_string(lastname):
-        return make_response(jsonify({
-            "Error": "First name is invalid"
-        }), 400)
-    
-    response = user_view.create_user(
-        firstname, lastname, othername, email, phonenumber, username, password
+    response = user_view.create_user(firstname, lastname, othername, email, phonenumber, username, password        
     )
     return make_response(jsonify({
             "Message": "User Created Successfully",
-            "details": response
+            "details": response,
+            "status": 201
         }), 201)
     
+
+@method1.route('/api/v1/login', methods=['POST'])
+def login():
+    """This is the login method"""
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+
+    return make_response(jsonify({
+        "status": "ok",
+        "data": {
+         "username": username,
+         "email": email
+        }
+    }), 200)
