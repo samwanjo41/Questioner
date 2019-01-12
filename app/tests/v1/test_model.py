8bcafb2de6de4483dfecd2219824d5eee231aee9
samwanjo41 <hulahoop41@gmail.com>
2019-01-12 17:35:51 +0300

    Modified test_model.py to add meetups tests

--- app/tests/v1/test_model.py
+++ app/tests/v1/test_model.py
@@ -38,4 +38,25 @@ class TestUsers(unittest.TestCase):
                                     content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
-   
+class MeetupTest(unittest.TestCase):
+    def setUp(self):
+       from app import create_app
+
+       self.app = create_app('testing')
+       self.client = self.app.test_client()
+       self.record1={
+            'topic' : "AI",
+            'location' : "iHub Upperhill,Nairobi Kenya " ,
+            'happeningOn' : "Sat 14th Jan 2019",
+        }
+       self.record2 = {
+            "meetup":"Ai",
+            "topic":"using AI to create jobs",
+            "status": "yes",
+            "name":"alan"
+        }
+    
+    def test_upcoming_meetups(self):
+        """user view upcoming meetups"""
+        response = self.client.get('/api/v1/meetups/upcoming', data= json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
