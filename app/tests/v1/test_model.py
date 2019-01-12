c2e3809f123005eb928d999bbadda318f1e5c922
samwanjo41 <hulahoop41@gmail.com>
2019-01-12 19:33:27 +0300

    Added tests for questio and meetups endpoints

--- app/tests/v1/test_model.py
+++ app/tests/v1/test_model.py
@@ -1,6 +1,7 @@
 import json
 import unittest 
 
+
 class TestUsers(unittest.TestCase):
 
     def setUp(self):
@@ -20,11 +21,10 @@ class TestUsers(unittest.TestCase):
                     "password": "kjney789" 
         }
 
-        self.data1 ={
+        self.data1 = { 
             'email': 'samwan@gmail.com',
-            'username':'a12ngdggd'
+            'username': 'a12ngdggd'
         }
-
 
     def test_register(self):
         response = self.client.post('api/v1/register',
@@ -38,25 +38,82 @@ class TestUsers(unittest.TestCase):
                                     content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
+
 class MeetupTest(unittest.TestCase):
     def setUp(self):
-       from app import create_app
+        from app import create_app
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.record1 = {
+            'topic': "AI",
+            'location': "iHub Upperhill,Nairobi Kenya ",
+            'happeningOn': "Sat 14th Jan 2019",
+        }
+        
+        self.meetup = {
+            "id": 1,
+            "createdOn": "Date",
+            "location": "Ihub",
+            "topic": "Google I/O",
+            "happeningOn": "Date",
+            "Tags": ["python", "Ihub"],
 
-       self.app = create_app('testing')
-       self.client = self.app.test_client()
-       self.record1={
-            'topic' : "AI",
-            'location' : "iHub Upperhill,Nairobi Kenya " ,
-            'happeningOn' : "Sat 14th Jan 2019",
         }
-       self.record2 = {
-            "meetup":"Ai",
-            "topic":"using AI to create jobs",
-            "status": "yes",
-            "name":"alan"
+
+        self.meetup3 = {
+            "images": "images",
+            "createdOn": "createdOn",
+            "location": "location",
+            "topic": "topic",
+            "happeningOn": "happeningOn",
+            "tags": ["python", "Ihub"]
         }
-    
+        
+
+      
     def test_upcoming_meetups(self):
         """user view upcoming meetups"""
-        response = self.client.get('/api/v1/meetups/upcoming', data= json.dumps(self.record1), content_type='application/json')
+        response = self.client.get('/api/v1/meetups/upcoming', data=json.dumps(self.record1), content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+    def test_specific_meetups(self):
+        """user view upcoming meetups"""
+        response = self.client.get('/api/v1/meetups/1', data=json.dumps(self.meetup), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_meetups(self):
+        """user view upcoming meetups"""
+        response = self.client.post('/api/v1/meetups', data=json.dumps(self.meetup3), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+
+class QuestionsTest(unittest.TestCase):
+   
+    def setUp(self):
+        from app import create_app
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.question1 = {
+            'user': 1,
+            'meetup': 1,
+            'title': "Entrance Ticket Price",
+            'body':  "How much will be paid by each person for entrance"
+        }
+        self.question2 = {
+            'user': 1,
+            'meetup': 1,
+            'title': "",
+            'body':  "How much will be paid by each person for entrance"
+        }
+
+        self.question3 = {
+            "createdBy": "createdBy",
+            "meetup": "meetup",
+            "title": "title",
+            "body": "body",
+            "votes": "votes"
+        }
+
+    def test_create_question(self):
+        response = self.client.post('/api/v1/question', data=json.dumps(self.question3), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
