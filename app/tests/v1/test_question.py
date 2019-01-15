f2b4484414e1b50551b784be09ac9594de502005
samwanjo41 <hulahoop41@gmail.com>
2019-01-15 07:54:53 +0300

    Created tests for question record

---
+++ app/tests/v1/test_question.py
@@ -0,0 +1,44 @@
+import json
+import unittest 
+
+
+class QuestionsTest(unittest.TestCase):
+   
+    def setUp(self):
+        from app import create_app
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.question1 = {
+            'user': 1,
+            'meetup': 1,
+            'title': "Entrance Ticket Price",
+            'body':  "How much will be paid by each person for entrance"
+        }
+        self.question2 = {
+            'user': 1,
+            'meetup': 1,
+            'title': "",
+            'body':  "How much will be paid by each person for entrance"
+        }
+
+        self.question3 = {
+            "createdBy": "createdBy",
+            "meetup": "meetup",
+            "title": "title",
+            "body": "body",
+            "votes": "votes"
+        }
+
+    def test_create_question(self):
+        response = self.client.post('/api/v1/question', data=json.dumps(self.question3), content_type='application/json')
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(result["status"], 201)
+
+    def test_get_all_question(self):
+        response = self.client.get('/api/v1/question')
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(result["status"], 201)
+
+    
